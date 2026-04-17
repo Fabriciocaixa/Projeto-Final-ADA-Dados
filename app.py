f593eff62541fd51d760f762cfaa684eeed1f0de
Fabriciocaixa <fabricio.l.gusmao@caixa.gov.br>
2026-04-17 11:55:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def healthcheck():
+    return {"status": "ok", "message": "Aplicação funcionando"}
+
+@app.route("/vendas")
+def vendas():
+    return {
+        "total_vendas": 150000,
+        "ticket_medio": 320.50
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
